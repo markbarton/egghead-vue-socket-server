994e026a9ee0bb33dc1c94706cc318eb1989e992
markbarton <markbarton@gmail.com>
2019-01-17 14:12:34 +0000

    latest

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ app.get('/notification/:message',
 )
 app.get('/binary',
     function (req, res) {
-
         fs.readFile(__dirname + '/cat.jpg', function (err, buf) {
-            // it's possible to embed binary data
-            // within arbitrarily-complex objects
             socket.emit('BINARY_NOTIFICATION', {
                 image: true,
                 buffer: buf.toString('base64')
@@ -40,12 +37,3 @@ http.listen(port);
 logger.info(`${pjson.name} Server Started >> `);
 logger.info(`running in ${process.env.NODE_ENV}`);
 logger.info(`running on port ${port}`);
-
-
-setInterval(function () {
-    socket.emit('PULSE', heartbeat());
-}, 1000);
-
-function heartbeat() {
-    return Math.ceil(Math.random() * (120 - 80) + 80);
-}
