bb827b2ee01a730e2fe7925016caf7d802a76cf4
markbarton <markbarton@gmail.com>
2019-01-17 12:14:06 +0000

    Update

--- server.js
+++ server.js
@@ -27,12 +27,3 @@ http.listen(port);
 logger.info(`${pjson.name} Server Started >> `);
 logger.info(`running in ${process.env.NODE_ENV}`);
 logger.info(`running on port ${port}`);
-
-
-setInterval(function () {
-    socket.emit('PULSE', heartbeat());
-}, 1000);
-
-function heartbeat() {
-    return Math.ceil(Math.random() * (120 - 80) + 80);
-}
