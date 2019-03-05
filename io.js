1f4b8af031957e131b61a60ff848d43e482f5ed4
markbarton <markbarton@gmail.com>
2019-03-05 17:31:46 +0000

    refactor + tidy

--- io.js
+++ io.js
@@ -6,6 +6,12 @@ const logger = require('./logger');
 
 let io = null;
 
+// Exported instantiated socket io object
 exports.io = function () {
   return io;
 };
+
+//  Intstantiate socket io object
+exports.initialize = function (server) {
+  io = sio(server);
+}
