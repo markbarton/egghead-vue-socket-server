fe56f70652c5e7790c0ba205275e5102eaa8aa30
markbarton <markbarton@gmail.com>
2019-03-08 17:16:13 +0000

    Refactor

--- io.js
+++ io.js
@@ -14,6 +14,19 @@ exports.initialize = function (server) {
   io.on('connection', (socket) => {
     logger.debug(`A user connected with ${socket.id}`);
 
+    socket.on('disconnect', function () {
+      // Updated to get real username
+      const user_data = ids.get(socket.id);
+      if(user_data){
+        logger.debug('USER DISCONNECTED ' + user_data.name)
+      }else{
+        // Might have started the server after  so Users are not registered
+        
+      }
+
+    })
+
+    // Sent from the UserProfile Vue Component
     socket.on('UPDATE_USER', function (data) {
       logger.debug(`UPDATE_USER triggered for ${data.name}`)
       // Map Socket ID with a User
@@ -23,10 +36,11 @@ exports.initialize = function (server) {
       });
       ids.set(socket.id, data);
 
-      // Also join a room / group
+      // Also join a room / group - inbuilt socket functionality
       socket.join(data.group);
     });
 
+    // Send from the ComposeMessage Vue component
     socket.on('SEND_MESSAGE', function (data) {
       // If we have a name then its to a person else group
       let recipient = '';
--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const pjson = require("./package.json");
 const path = require('path');
 const http = require('http').Server(app);
 const logger = require('./logger');
-const fs = require('fs');
 
 require('./io').initialize(http);
-const socket = require('./io').io();
 
 const port = process.env.PORT || 8500;
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,12 +18,3 @@ http.listen(port);
 logger.info(`${pjson.name} Server Started >> `);
 logger.info(`running in ${process.env.NODE_ENV}`);
 logger.info(`running on port ${port}`);
-
-
-/* setInterval(function () {
-    socket.emit('PULSE', heartbeat());
-}, 1000);
-
-function heartbeat() {
-    return Math.ceil(Math.random() * (120 - 80) + 80);
-} */
