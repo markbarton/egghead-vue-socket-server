af79e011a1d5396f6b24f738b3da920294abb341
Mark Barton <markbarton@Marks-Mac-mini.local>
2019-04-13 13:33:37 +0100

    changes to reflect the video

--- io.js
+++ io.js
@@ -1,17 +1,13 @@
-/**
- * Created by markb on 30/06/2017.
- */
 const sio = require('socket.io');
 const logger = require('./logger');
 
 let io = null;
 
-// Exported instantiated socket io object
-exports.io = function () {
-  return io;
-};
-
 //  Intstantiate socket io object
 exports.initialize = function (server) {
-  io = sio(server);
+    io = sio(server);
+}
+
+exports.io = function (){
+    return io;
 }
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const dotenv = require("dotenv").config({
     path: "variables.env"
 });
+
 const express = require('express');
 const app = express();
 const pjson = require("./package.json");
 const path = require('path');
-const server = require('http').Server(app);
-const logger = require('./logger');
 const port = process.env.PORT || 8500;
+
+const server = require('http').Server(app);
 const io = require('./io').initialize(server);
+const logger = require('./logger');
 
 // Static file service
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,3 +21,4 @@ server.listen(port);
 logger.info(`${pjson.name} Server Started >> `);
 logger.info(`running in ${process.env.NODE_ENV}`);
 logger.info(`running on port ${port}`);
+
