0dc60d0cf8450ed6fda30ef9f6a32677375d9dd4
Mark Barton <markbarton@marks-mac-mini.home>
2019-05-12 17:11:58 +0100

    disconnect

--- io.js
+++ io.js
@@ -15,44 +15,34 @@ exports.initialize = function (server) {
     logger.debug(`A user connected with ${socket.id}`);
 
     socket.on('disconnect', function () {
-      // Updated to get real username
       const user_data = ids.get(socket.id);
       if(user_data){
         logger.debug('USER DISCONNECTED ' + user_data.name)
-      }else{
-        // Might have started the server after  so Users are not registered
-        
       }
-
     })
 
-    // Sent from the UserProfile Vue Component
-    socket.on('UPDATE_USER', function (data) {
+    socket.on('UPDATE_USER', function(data){
       logger.debug(`UPDATE_USER triggered for ${data.name}`)
-      // Map Socket ID with a User
+    
       users.set(data.name, {
         socket_id: socket.id,
         ...data
       });
-      ids.set(socket.id, data);
-
-      // Also join a room / group - inbuilt socket functionality
+      ids.set(socket.id,data);
       socket.join(data.group);
-    });
-
-    // Send from the ComposeMessage Vue component
-    socket.on('SEND_MESSAGE', function (data) {
-      // If we have a name then its to a person else group
+    })
+  
+    socket.on('SEND_MESSAGE', function (data){
       let recipient = '';
-      if (data.name) {
+      if(data.name){
         const user = users.get(data.name);
         recipient = user.socket_id;
-      } else {
+      }else{
         recipient = data.group;
       }
-      logger.debug(`POPUP_NOTIFICATION triggered for ${recipient}`)
+      logger.debug(`POPUP_NOTIFICATION triggered for ${recipient}`);
       io.to(recipient).emit('POPUP_NOTIFICATION', data);
-    });
+    })
 
   });
 };
